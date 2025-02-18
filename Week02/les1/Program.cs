c8c079ab7b134f7f6da2623614dadd15fb9fc862
Barld Boot <booba@hr.nl>
2025-02-18 12:02:30 +0100

    add lesson 2.1

---
+++ Week02/les1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+static class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the program");
+
+        Car myCar = new Car("Renault Clio", "red", 7.0, false);
+        Car bmw = new Car("BMW M3", "Black", 3.0, false);
+
+        PrintInformation(myCar);
+    }
+
+    private static void PrintInformation(Car car)
+    {
+        Console.WriteLine(car.Color);
+        Console.WriteLine(car.GetDescription());
+    }
+}
