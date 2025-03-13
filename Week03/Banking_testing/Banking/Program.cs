cb5e420e4282597fa7f3c5dab13af65fe4dfc2aa
Barld Boot <booba@hr.nl>
2025-03-13 09:16:51 +0100

    Add banking testing project

---
+++ Week03/Banking_testing/Banking/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Account account = new Account();
+try
+{
+    account.Deposit(100);
+    double withdrawAmount = double.Parse(Console.ReadLine());
+
+    account.Withdraw(withdrawAmount);
+}
+catch(FormatException ex)
+{
+    Console.WriteLine("Je hebt het verkeerde format gebruikt");
+}
+catch(InvalidOperationException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("Dit wordt altijd uitgevoerd!");
+}
+
+
+
